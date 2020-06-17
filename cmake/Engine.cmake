dede9eeebad5a4468f1563f5d209bb342e63d976
Rudy Alex Kohn <s133235@student.dtu.dk>
2020-06-17 15:33:47 +0200

    Fixed compile definitions

--- cmake/Engine.cmake
+++ cmake/Engine.cmake
@@ -1,7 +1,7 @@
 # Contains various settings for the engine when it is compiled.
 function(enable_engine_options)
     option(NO_EVAL_LAZY_THRESHOLD "Enable lazy (value) evaluation threshold" OFF)
-    if (EVAL_LAZY_THRESHOLD)
+    if (NO_EVAL_LAZY_THRESHOLD)
         add_definitions("-DNO_LAZY_EVAL_THRESHOLD")
     endif()
 
