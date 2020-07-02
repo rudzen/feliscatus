a7018f2fbba191eb82fdc24a446a35b9738de837
Rudy Alex Kohn <rudzen@gmail.com>
2020-07-02 23:15:56 +0200

    Simplified SEE slightly

--- src/see.cpp
+++ src/see.cpp
@@ -19,60 +19,124 @@
 */
 
 #include "board.h"
-#include "material.h"
 #include "bitboard.h"
 #include "magic.h"
 #include "types.h"
 
 namespace {
 
+using CurrentPieceType = std::array<PieceType, COL_NB>;
+using CurrentPieces = std::array<Bitboard, COL_NB>;
+
+struct SeeData final {
+  CurrentPieceType current_pt{};
+  CurrentPieces current_pc{};
+};
+
 constexpr int SEE_INVALID_SCORE = -5000;
 
-constexpr int material_change(const Move move) {
+constexpr auto material_change(const Move move) {
   return (is_capture(move) ? piece_value(move_captured(move)) : 0) + (is_promotion(move) ? (piece_value(move_promoted(move)) - piece_value(Pawn)) : 0);
 }
 
-constexpr Piece next_to_capture(const Move move) {
+constexpr auto next_to_capture(const Move move) {
   return is_promotion(move) ? move_promoted(move) : move_piece(move);
+}
+
+auto get_from_sq (const Bitboard bb, SeeData &data, const Color stm) {
+  const auto from = lsb(bb);
+  data.current_pc[stm] &= ~bit(from);
+  return std::make_optional(from);
+}
+
+std::optional<Square> lookup_best_attacker(SeeData &data, const Square to, const Color side, const Board *b) {
+  // "Best" == "Lowest piece value"
+
+  const auto occupied = b->pieces();
+  Bitboard bb;
+
+  switch (data.current_pt[side])
+  {
+  case Pawn:
+    bb = data.current_pc[side] & pawn_attacks_bb(~side, to);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    ++data.current_pt[side];
+    data.current_pc[side] = b->pieces(Knight, side);
+    [[fallthrough]];
+  case Knight:
+    bb = data.current_pc[side] & piece_attacks_bb<Knight>(to);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    ++data.current_pt[side];
+    data.current_pc[side] = b->pieces(Bishop, side);
+    [[fallthrough]];
+
+  case Bishop:
+    bb = data.current_pc[side] & piece_attacks_bb<Bishop>(to, occupied);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    ++data.current_pt[side];
+    data.current_pc[side] = b->pieces(Rook, side);
+    [[fallthrough]];
+  case Rook:
+    bb = data.current_pc[side] & piece_attacks_bb<Rook>(to, occupied);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    ++data.current_pt[side];
+    data.current_pc[side] = b->pieces(Queen, side);
+    [[fallthrough]];
+  case Queen:
+    bb = data.current_pc[side] & piece_attacks_bb<Queen>(to, occupied);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    ++data.current_pt[side];
+    data.current_pc[side] = b->pieces(King, side);
+    [[fallthrough]];
+  case King:
+    bb = data.current_pc[side] & piece_attacks_bb<King>(to);
+    if (bb)
+      return get_from_sq(bb, data, side);
+    break;
+  default:
+    break;
+  }
+
+  return std::nullopt;
 }
 
 }
 
 int Board::see_move(const Move move) {
-  int score;
   perform_move(move);
 
-  const auto us   = move_side(move);
-  const auto them = ~us;
-
-  if (!is_attacked(king_square[us], them))
-  {
-    init_see_move();
-    score = see_rec(material_change(move), next_to_capture(move), move_to(move), them);
-  } else
-    score = SEE_INVALID_SCORE;
+  const auto us    = move_side(move);
+  const auto them  = ~us;
+  const auto score = !is_attacked(king_square[us], them)
+                   ? see_rec(material_change(move), next_to_capture(move), move_to(move), them)
+                   : SEE_INVALID_SCORE;
 
   unperform_move(move);
   return score;
 }
 
 int Board::see_last_move(const Move move) {
-  init_see_move();
   return see_rec(material_change(move), next_to_capture(move), move_to(move), ~move_side(move));
 }
 
 int Board::see_rec(const int mat_change, const Piece next_capture, const Square to, const Color side_to_move) {
-  const auto rr = relative_rank(side_to_move, to);
 
+  SeeData data{{Pawn, Pawn}, {pieces(Pawn, WHITE), pieces(Pawn, BLACK)}};
+  const auto rr = relative_rank(side_to_move, to);
   Move move;
 
   do
   {
-    const auto from = lookup_best_attacker(to, side_to_move);
+    const auto from = lookup_best_attacker(data, to, side_to_move, this);
     if (!from)
       return mat_change;
 
-    const auto current_pt = current_piece[side_to_move];
+    const auto current_pt = data.current_pt[side_to_move];
 
     move = current_pt == Pawn && rr == RANK_8
          ? init_move<PROMOTION | CAPTURE>(make_piece(current_pt, side_to_move), next_capture, from.value(), to, make_piece(Queen, side_to_move))
@@ -91,73 +155,4 @@ int Board::see_rec(const int mat_change, const Piece next_capture, const Square 
   unperform_move(move);
 
   return score < 0 ? mat_change + score : mat_change;
-}
-
-std::optional<Square> Board::lookup_best_attacker(const Square to, const Color side) {
-  // "Best" == "Lowest piece value"
-
-  Bitboard b;
-
-  const auto get_from_sq = [&](const Bitboard bb)
-  {
-    const auto from = lsb(bb);
-    current_piece_bitboard[side] &= ~bit(from);
-    return std::make_optional(from);
-  };
-
-  switch (current_piece[side])
-  {
-  case Pawn:
-    b = current_piece_bitboard[side] & pawn_attacks_bb(~side, to);
-    if (b)
-      return get_from_sq(b);
-    ++current_piece[side];
-    current_piece_bitboard[side] = pieces(Knight, side);
-    [[fallthrough]];
-  case Knight:
-    b = current_piece_bitboard[side] & piece_attacks_bb<Knight>(to);
-    if (b)
-      return get_from_sq(b);
-    ++current_piece[side];
-    current_piece_bitboard[side] = pieces(Bishop, side);
-    [[fallthrough]];
-
-  case Bishop:
-    b = current_piece_bitboard[side] & piece_attacks_bb<Bishop>(to, occupied);
-    if (b)
-      return get_from_sq(b);
-    ++current_piece[side];
-    current_piece_bitboard[side] = pieces(Rook, side);
-    [[fallthrough]];
-  case Rook:
-    b = current_piece_bitboard[side] & piece_attacks_bb<Rook>(to, occupied);
-    if (b)
-      return get_from_sq(b);
-    ++current_piece[side];
-    current_piece_bitboard[side] = pieces(Queen, side);
-    [[fallthrough]];
-  case Queen:
-    b = current_piece_bitboard[side] & piece_attacks_bb<Queen>(to, occupied);
-    if (b)
-      return get_from_sq(b);
-    ++current_piece[side];
-    current_piece_bitboard[side] = pieces(King, side);
-    [[fallthrough]];
-  case King:
-    b = current_piece_bitboard[side] & piece_attacks_bb<King>(to);
-    if (b)
-      return get_from_sq(b);
-    break;
-  default:
-    break;
-  }
-
-  return std::nullopt;
-
-}
-
-void Board::init_see_move() {
-  current_piece.fill(Pawn);
-  current_piece_bitboard[WHITE] = pieces(Pawn, WHITE);
-  current_piece_bitboard[BLACK] = pieces(Pawn, BLACK);
 }
